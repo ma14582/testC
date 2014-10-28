49512f7e72f126c3905801c92b1fcc7e487ea8e3
maria <maria@localhost.localdomain>
2014-10-28 21:49:51 +0000

    - first commit

---
+++ testC/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: maria
+ *
+ * Created on 28 October 2014, 21:49
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    printf("Hello World!");
+    return (EXIT_SUCCESS);
+}
+
